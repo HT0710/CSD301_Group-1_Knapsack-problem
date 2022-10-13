de121f8c8fad17e70048ec23445c24cc46dd6113
Tra Nguyen Huu Nghia <nghiatnh02@gmail.com>
2022-10-13 15:17:36 +0700

    Update GUI code
    + I added Utility file
    + I add more comment in GUI and Utility files

--- UIDesign/GUI.py
+++ UIDesign/GUI.py
@@ -1,12 +1,14 @@
-from concurrent.futures import thread
-import json
-import math
+import sys
+import os
+from multiprocessing import Process, Manager
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 from random import random
-from statistics import quantiles
 from time import time
-from UI_mainwindow import Ui_mainwindow
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QFileDialog, QWidget, QTableWidgetItem, QMessageBox
+from UIDesign.Utility import *
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+import Algorithms.Algorithms as algorithms
 
 
 class GUI(Ui_mainwindow):
@@ -18,8 +20,10 @@ class GUI(Ui_mainwindow):
         self.setupUi(self.mainwindow)
         self.setupSignal()
 
-    # Add event to buttons and menu items
     def setupSignal(self) -> None:
+        '''
+        Add event to buttons and menu items
+        '''
         # Buttons
         self.btn_solve.clicked.connect(self.btn_solve_clicked)
         self.btn_addInput.clicked.connect(self.btn_addInput_clicked)
@@ -47,32 +51,30 @@ class GUI(Ui_mainwindow):
             self.actionChangeAlgorithm_triggered)
         self.actionRandom.triggered.connect(self.btn_randomInput_clicked)
 
-    # Show the window
     def show(self) -> None:
+        '''
+        Show the main window
+        '''
         self.mainwindow.show()
 
     '''
     ---- When buttons click ----
     '''
 
-    # Button solve clicked
     def btn_solve_clicked(self, checked) -> None:
-        import sys
-        import os
-        SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-        sys.path.append(os.path.dirname(SCRIPT_DIR))
-
-        import Algorithms.Algorithms as algorithms
+        '''
+        Solve Knapsack Problem with the given algorithm and show output to table
+        '''
 
         algorithm = None
         try:
-            data = self.exportInputToJson()
+            data = exportInputToJson(self)
             C = data["maximum weight"]
             W = data["table"]["Weight"]
             P = data["table"]["Price"]
             indexes = []
         except:
-            self.showMessageBox("Error", QMessageBox.critical,
+            showMessageBox("Error", QMessageBox.critical,
                                 "Error when read input!")
             return
         try:
@@ -85,62 +87,85 @@ class GUI(Ui_mainwindow):
                     algorithm = algorithms.BranchAndBound
                 case "Brute Force":
                     algorithm = algorithms.BruteForce
+                case "Brute Force with Memorization":
+                    algorithm = algorithms.BruteForceMemorization
                 case "Genetic":
                     algorithm = algorithms.Genetic
                 case _:
                     raise Exception("Can not find the algorithm!")
 
-            t1 = time()
-            indexes = algorithm.findSolution(C, W, P)
-            t2 = time()
+            indexes = Manager().list()
+            def do():
+                indexes1 = algorithm.findSolution(C, W, P)
+                for i in indexes1:
+                    indexes.append(i)
+            
 
-            self.addDataToOutputTable({
+            prc = Process(target=do)
+            t1 = time()
+            prc.start()
+            prc.join(timeout=self.spb_timeOut.value())
+            t2 = time()
+            prc.terminate()
+
+            addDataToOutputTable(self, {
                 "table": {
                     "Weight": [W[x] for x in indexes],
                     "Price": [P[x] for x in indexes],
                 }
             })
-
+            
             self.lb_time.setText("Time: {} s".format(t2 - t1))
-            self.lb_status.setText("Status: completed!")
+            self.lb_status.setText("Status: " + ("completed!" if t2 - t1 <= 10 else "time out!"))
+
         except Exception as e:
             self.lb_status.setText("Status: error!")
-            self.showMessageBox("Error", QMessageBox.critical, str(e))
+            showMessageBox(self, "Error", QMessageBox.critical, str(e))
 
-    # Add new row to input table and focus last row
+            
     def btn_addInput_clicked(self, checked) -> None:
+        '''
+        Add new row to input table and focus last row
+        '''
         self.tb_input.insertRow(self.tb_input.rowCount())
         self.tb_input.clearSelection()
         self.tb_input.selectRow(self.tb_input.rowCount()-1)
         self.tb_input.setFocus()
 
-    # Delete selected rows in input table in descent order
     def btn_deleteInput_clicked(self, checked) -> None:
+        '''
+        Delete selected rows in input table in descent order
+        '''
         rows = list(set([index.row()
                     for index in self.tb_input.selectedIndexes()]))
         rows.reverse()
         for index in rows:
             self.tb_input.removeRow(index)
 
-    # Convert input data to JSON then save to file
     def btn_exportInput_clicked(self, checked) -> None:
-        fileName = self.saveFileDialog()
+        '''
+        Convert input data to JSON then save to file
+        '''
+        fileName = saveFileDialog()
         if fileName:
             try:
-                jsonData = self.exportInputToJson()
-                self.saveData(fileName, jsonData)
-                self.showMessageBox(
+                jsonData = exportInputToJson(self)
+                saveData(fileName, jsonData)
+                showMessageBox( 
                     message="Export input data successfully!", boxType=QMessageBox.information)
             except:
-                self.showMessageBox(
+                showMessageBox( 
                     message="Error when export input data!", boxType=QMessageBox.critical)
 
     def btn_randomInput_clicked(self, checked) -> None:
+        '''
+        Generate the number of (max weight) inputs with value in range of maximum weight and add them into input table
+        '''
         max_weights = self.spb_maximumWeight.value()
         weights = [int(random() * max_weights + 1) for i in range(max_weights)]
         price = [int(random() * max_weights + 1) for i in range(max_weights)]
         
-        self.addDataToInputTable({
+        addDataToInputTable(self, {
             "table": {
                 "Weight": weights,
                 "Price": price,
@@ -151,165 +176,56 @@ class GUI(Ui_mainwindow):
     ---- Actions in File Menu ----
     '''
 
-    # Close window
     def actionExit_triggered(self, checked) -> None:
-        if self.showConfirmBox(message="Do you sure to exit?") == QMessageBox.Yes:
+        '''
+        Close window
+        '''
+        if showConfirmBox(message="Do you sure to exit?") == QMessageBox.Yes:
             self.mainwindow.close()
 
-    # Read and import data to input table
     def actionImportDataToExist_triggered(self, checked) -> None:
-        fileName = self.openFileNameDialog()
+        '''
+        Read and import data to input table
+        '''
+        fileName = openFileNameDialog()
         if fileName:
             try:
-                data = self.readData(fileName)
-                self.addDataToInputTable(data)
+                data = readData(fileName)
+                addDataToInputTable(self, data)
             except:
-                self.showMessageBox(
+                showMessageBox( 
                     message="Error when import data!", boxType=QMessageBox.critical)
 
-    # Read data, then clear table and import data to input table
     def actionImportDataToNew_triggered(self, checked) -> None:
-        fileName = self.openFileNameDialog()
+        '''
+        Read data, then clear table and import data to input table
+        '''
+        fileName = openFileNameDialog()
         if fileName:
             try:
                 self.tb_input.setRowCount(0)
-                data = self.readData(fileName)
-                self.addDataToInputTable(data)
+                data = readData(fileName)
+                addDataToInputTable(self, data)
             except:
-                self.showMessageBox(
+                showMessageBox( 
                     message="Error when import data!", boxType=QMessageBox.critical)
 
     '''
-    Actions in Edit Menu
+    ---- Actions in Edit Menu ----
     '''
 
-    # Focus to SpinBox Maximum Weight
     def actionChangeMaximumWeight_triggered(self, checked) -> None:
+        '''
+        Focus to SpinBox Maximum Weight
+        '''
         self.spb_maximumWeight.setFocus()
 
-    # Show combobox Algorithm and focus it
     def actionChangeAlgorithm_triggered(self, checked) -> None:
+        '''
+        Show combobox Algorithm and focus it
+        '''
         self.cbb_algorithm.showPopup()
         self.cbb_algorithm.setFocus()
-
-    '''
-    ---- Utilities functions ----
-    '''
-
-    # Get column index from column name
-    def getColumnIndex(self) -> dict:
-        result = dict()
-        for i in range(self.tb_input.columnCount()):
-            result[self.tb_input.horizontalHeaderItem(i).text()] = i
-        return result
-
-    # Show open file dialog and return full path of selected file
-    def openFileNameDialog(self) -> str:
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self.modalWidget(
-        ), "Import Data", "", "JSON Files (*.json);;Input Files (*.inp);;All Files (*)", options=options)
-        return fileName
-
-    # Show save file dialog and return full path of saved file
-    def saveFileDialog(self) -> str:
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self.modalWidget(
-        ), "Export Data", "", "JSON Files (*.json);;Input Files (*.inp);;All Files (*)", options=options)
-        return fileName
-
-    # Read JSON Data
-    def readData(self, fileName: str) -> dict:
-        import json
-        with open(fileName, "r") as f:
-            data: dict = json.load(f)
-
-        if data:
-            return data
-        return dict()
-
-    # Save data to fileName
-    def saveData(self, fileName: str, jsonData: str) -> None:
-        with open(fileName, 'w') as f:
-            json.dump(jsonData, f)
-
-    # Convert data from combobox algorithm, spinbox maximum weight and input table to JSON string
-    def exportInputToJson(self) -> dict:
-        columns = self.getColumnIndex()
-        jsonData = dict()
-        jsonData["algorithm"] = self.cbb_algorithm.currentText()
-        jsonData["maximum weight"] = self.spb_maximumWeight.value()
-        for key in columns:
-            columns[key] = [int(self.tb_input.item(i, columns[key]).text())
-                            for i in range(self.tb_input.rowCount())]
-        jsonData["table"] = columns
-        return jsonData
-
-    # Add data to combobox algorithm, spinbox maximum weight and input table
-    def addDataToInputTable(self, jsonData: dict) -> None:
-        algorithm = jsonData.get("algorithm", self.cbb_algorithm.currentText())
-        self.cbb_algorithm.setCurrentText(algorithm)
-        maxWeight = jsonData.get(
-            "maximum weight", self.spb_maximumWeight.value())
-        self.spb_maximumWeight.setValue(maxWeight)
-        table: dict = jsonData.get("table", {})
-        weights: list = table.get("Weight", [])
-        values: list = table.get("Price", [])
-        columns: dict = self.getColumnIndex()
-        tableRow = self.tb_input.rowCount()
-        self.tb_input.setRowCount(
-            tableRow + max(len(weights), len(quantities)))
-        for i in range(len(weights)):
-            self.tb_input.setItem(
-                tableRow + i, columns["Weight"], QtWidgets.QTableWidgetItem(str(weights[i])))
-        for i in range(len(values)):
-            self.tb_input.setItem(
-                tableRow + i, columns["Price"], QtWidgets.QTableWidgetItem(str(values[i])))
-
-    # Add data to combobox algorithm, spinbox maximum weight and input table
-    def addDataToOutputTable(self, jsonData: dict) -> None:
-        table: dict = jsonData.get("table", {})
-        weights: list = table.get("Weight", [])
-        values: list = table.get("Price", [])
-        columns: dict = self.getColumnIndex()
-
-        self.tb_output.setRowCount(
-            max(len(weights), len(values)) + 1)
-        for i in range(len(weights)):
-            self.tb_output.setItem(
-                i, columns["Weight"], QtWidgets.QTableWidgetItem(str(weights[i])))
-        for i in range(len(values)):
-            self.tb_output.setItem(
-                i, columns["Price"], QtWidgets.QTableWidgetItem(str(values[i])))
-
-        self.tb_output.setVerticalHeaderLabels(
-            [str(x + 1) for x in range(self.tb_output.rowCount())])
-        self.tb_output.setVerticalHeaderItem(
-            self.tb_output.rowCount() - 1, QtWidgets.QTableWidgetItem("Total"))
-        self.tb_output.setItem(self.tb_output.rowCount(
-        ) - 1, columns["Weight"], QtWidgets.QTableWidgetItem(str(sum(weights))))
-        self.tb_output.setItem(self.tb_output.rowCount(
-        ) - 1, columns["Price"], QtWidgets.QTableWidgetItem(str(sum(values))))
-        
-
-    # Create dialog or message Widget
-    def modalWidget(self, width: int = 400, height: int = 100) -> QWidget:
-        wget = QWidget()
-        wget.setFixedSize(width, height)
-        wget.move(self.mainwindow.x() + int(self.mainwindow.width() / 2) - int(wget.width() / 2),
-                  self.mainwindow.y() + int(self.mainwindow.height() / 2) - int(wget.height() / 2))
-
-    # Show messagebox that only display message and OK button
-    def showMessageBox(self, title: str = "Notification", boxType: QMessageBox.StandardButton = QMessageBox.information, message: str = "") -> None:
-        boxType(self.modalWidget(), title, message, QMessageBox.Ok)
-
-    # Show confirm box that return Yes or No
-    def showConfirmBox(self, title: str = "Confirm", message: str = "") -> QMessageBox.StandardButton:
-        reply = QMessageBox.question(
-            self.modalWidget(), title, message, QMessageBox.No | QMessageBox.Yes)
-        return reply
-
 
 # Run GUI app
 if __name__ == "__main__":
@@ -317,4 +233,4 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     ui = GUI()
     ui.show()
-    app.exec_()
+    sys.exit(app.exec_())
